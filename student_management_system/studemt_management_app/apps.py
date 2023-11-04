8b1b88a967534c6067e8b59a97e5a21f777dc58d
R.M.Y.P.Rathnayake <122531660+asp2020100@users.noreply.github.com>
2023-11-05 02:08:38 +0530

    initial setup
    DB Created on LocalHost and Some Tables are added to the Django Model.py file.

---
+++ student_management_system/studemt_management_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StudemtManagementAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'studemt_management_app'
---
+++ student_management_system/studemt_management_app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+# Create your models here.
+class Admin(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=255)
+    email=models.CharField(max_length=255)
+    password=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+    
+class Courses(models.Model):
+    id=models.AutoField(primary_key=True)
+    course_name=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class Students(models.Model):
+    id=models.AutoField(primary_key=True)
+    admin=models.OneToOneField(CustomUser,on_delete=models.CASCADE)
+    gender=models.CharField(max_length=255)
+    profile_pic=models.FileField()
+    address=models.TextField()
+    course_id=models.ForeignKey(Courses,on_delete=models.DO_NOTHING)
+    session_start_year=models.DateField()
+    session_end_year=models.DateField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
