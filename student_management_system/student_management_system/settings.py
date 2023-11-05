673645275f26fb5676056fbdcb1cb817e96e24b3
R.M.Y.P.Rathnayake <122531660+asp2020100@users.noreply.github.com>
2023-11-05 22:24:28 +0530

    Admin Panel Plugins Addded!
    https://github.com/ColorlibHQ/AdminLTE/releases/tag/v3.2.0

--- student_management_system/student_management_system/settings.py
+++ student_management_system/student_management_system/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+
+STATIC_URL="/static/"
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
 
 # Application definition
 
